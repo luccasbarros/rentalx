3733455cddd7ec2fc4b816c1bfb8f2c78b2af145
Luccas <luccas.barros@shareprime.com.br>
2021-04-28 06:29:44 -0300

    initial commit

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
+export const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
+
+categoriesRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+
+  categoriesRepository.create({ name, description });
+
+  return response.status(201).send();
+});
