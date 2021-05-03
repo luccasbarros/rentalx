811e6bb258dcf0031c92f2cd9a0c42f3edd6f7c0
Luccas <luccas.barros@shareprime.com.br>
2021-05-03 09:30:27 -0300

    feat: creating stream to file read

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,17 @@
+import fs from "fs";
+import csvParse from "csv-parse";
+
 class ImportCategoryUseCase {
   execute(file: Express.Multer.File): void {
-    console.log(file);
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
   }
 }
 
