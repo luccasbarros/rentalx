7e07dae196ee436f85f8b93ce4ae96124d7a6cbc
Luccas <luccas.barros@shareprime.com.br>
2021-05-06 18:05:29 -0300

    hotfix: fixing database index connection and adding database config on docker compose

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
 import swaggerFile from "./swagger.json";
+import "./database";
 import { router } from "./routes/";
 
 const app = express();
