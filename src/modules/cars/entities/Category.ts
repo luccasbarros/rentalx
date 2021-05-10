398901390d31c5d053f3480ddf9cb76545d3f90e
Luccas <luccas.barros@shareprime.com.br>
2021-05-10 12:49:25 -0300

    config: entities configurations with notation

--- src/modules/cars/entities/Category.ts
+++ src/modules/cars/entities/Category.ts
@@ -1,9 +1,18 @@
 import { v4 as uuidv4 } from "uuid";
+import { Entity, PrimaryColumn, Column, CreateDateColumn } from "typeorm";
 
+@Entity("Categories")
 class Category {
+  @PrimaryColumn()
   id?: string;
+
+  @Column()
   name: string;
+
+  @Column()
   description: string;
+
+  @CreateDateColumn()
   created_at: Date;
 
   constructor() {
