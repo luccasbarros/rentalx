09f3e26ca67e81f610a7f2bc8c2a5706bdafc9f9
Luccas <luccasbarros@protonmail.com>
2021-05-31 15:41:41 -0300

    feat: add bcrypt

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from "tsyringe";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { hash } from "bcrypt";
 
 @injectable()
 class CreateUserUseCase {
