5a841ebd67e0615e10426804dc438c4d29f86e06
Luccas <luccasbarros@protonmail.com>
2021-06-08 08:19:20 -0300

    feat: remove avatar when change to another

--- src/modules/accounts/useCases/updateUserAvatar/updateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/updateUserAvatarUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { deleteFile } from "../../../../utils/file";
 
 interface IRequest {
   userId: string;
@@ -14,6 +15,10 @@ class UpdateUserAvatarUseCase {
   ) {}
   async execute({ userId, avatarFile }: IRequest): Promise<void> {
     const user = await this.usersRepository.findById(userId);
+
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar/${user.avatar}`);
+    }
     user.avatar = avatarFile;
 
     await this.usersRepository.create(user);
