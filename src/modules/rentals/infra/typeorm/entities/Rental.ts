18daa95a223cee46dfabe9685916a8a580c1e5d8
Luccas Barros <luccasbarros@pm.me>
2021-09-15 07:08:04 -0300

    refactor: add car relation in user rental list

--- src/modules/rentals/infra/typeorm/entities/Rental.ts
+++ src/modules/rentals/infra/typeorm/entities/Rental.ts
@@ -1,7 +1,10 @@
+import { Car } from "../../../../../modules/cars/infra/typeorm/entities/Cars";
 import {
   Column,
   CreateDateColumn,
   Entity,
+  JoinColumn,
+  ManyToOne,
   PrimaryColumn,
   UpdateDateColumn,
 } from "typeorm";
@@ -11,6 +14,10 @@ import { v4 as uuidv4 } from "uuid";
 class Rental {
   @PrimaryColumn()
   id: string;
+
+  @ManyToOne(() => Car)
+  @JoinColumn({ name: "car_id" })
+  car: Car;
 
   @Column()
   car_id: string;
