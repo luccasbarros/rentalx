bc5842931974bb0dbd964e3d37774df544f0afa8
Luccas Barros <luccasbarros@pm.me>
2021-09-18 13:34:56 -0300

    feat: reset user password

--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,10 +1,12 @@
 import { SendForgotPasswordMailController } from "../../../../modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController";
 import { Router } from "express";
+import { ResetPasswordController } from "../../../../modules/accounts/useCases/resetPassword/ResetPasswordController";
 
 const passwordRoutes = Router();
 
 const sendForgotPasswordMailController = new SendForgotPasswordMailController();
-
+const resetPasswordController = new ResetPasswordController();
 passwordRoutes.post("/forgot", sendForgotPasswordMailController.handle);
+passwordRoutes.post("/reset", resetPasswordController.handle);
 
 export { passwordRoutes };
