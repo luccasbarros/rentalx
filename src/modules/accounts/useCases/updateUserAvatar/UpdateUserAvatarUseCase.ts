2bbbcb17e58e5350b09862b9d70c6761bc61f487
Luccas Barros <luccasbarros@pm.me>
2021-09-18 15:04:35 -0300

    refactor: added storage provider to update avatar use case

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,4 +1,5 @@
-import { deleteFile } from "@utils/file";
+import { IStorageProvider } from "../../../../shared/container/providers/StorageProvider/IStorageProvider";
+import { deleteFile } from "../../../../utils/file";
 import { inject, injectable } from "tsyringe";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -11,14 +12,19 @@ interface IRequest {
 class UpdateUserAvatarUseCase {
   constructor(
     @inject("UsersRepository")
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+    @inject("StorageProvider")
+    private storageProvider: IStorageProvider
   ) {}
   async execute({ userId, avatarFile }: IRequest): Promise<void> {
     const user = await this.usersRepository.findById(userId);
 
     if (user.avatar) {
-      await deleteFile(`./tmp/avatar/${user.avatar}`);
+      await this.storageProvider.delete(user.avatar, "avatar");
     }
+
+    await this.storageProvider.save(avatarFile, "avatar");
+
     user.avatar = avatarFile;
 
     await this.usersRepository.create(user);
