e296398b0e84cbdb8ce2825973cb89cb96c2e525
Luccas Barros <luccasbarros@pm.me>
2021-09-18 15:05:38 -0300

    fix: removed upload function from upload avatar multer func

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -7,7 +7,7 @@ import multer from "multer";
 
 const usersRoutes = Router();
 
-const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
+const uploadAvatar = multer(uploadConfig);
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
